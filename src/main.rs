518947a8d3a7d09a35c9a6263e12e20a8a0aa8c5
allexanderxha <allexanderxha@icloud.com>
2026-06-11 09:30:10 +0000

    * Fixes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![no_std]
 #![no_main]
 
+use core::panic::PanicInfo;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
 
-#[no_mangle] // Don't mangle the name of this function
+#[unsafe(no_mangle)] // Don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // kernel logic will begin here
     loop {}
