1b1e2261e0421909bf6dd417f41447b48f7a33b9
allexanderxha <allexanderxha@icloud.com>
2026-06-11 09:49:09 +0000

    * Default ACCT

---
+++ src/kernel/acct.rs
@@ -0,0 +1,49 @@
+/* ACCT-RS
+ *
+ * BSD Processing for RLUX-OS.
+ *
+ * Author: Allexander Bergmans <al@libregrad.pp.ua>
+ *
+ * Some code based on the source of:
+ * https://github.com/torvalds/linux/blob/master/kernel/acct.c
+ *
+ * This file implements a BSD-style process accounting. Whenever
+ * a process exists, an accounting record of type ´struct acct´ is
+ * writen to the file specified with the acct() system call.
+ *
+ * (C) Copyright 2025-2026 - Allexander Bergmans - Libregrad.
+ *
+ * 
+ */
+
+// =========================================================
+#![allow(non_camel_case_types)]
+// ==========================================================
+// The following imports are like 
+// linux/slab.h. This is inspired
+// by the Linux Kernel source code
+// located @:
+// https://github.com/torvalds/linux/blob/master/kernel/acct.c
+// ===========================================================
+use kernel::alloc::flags;
+use kernel::alloc:KBox;
+use kernel::uapi::acct;
+// ===========================================================
+// comp_t is a 16-bit "floating" point number with a 3-bit
+// base-8 exponent and a 13-bit fraction.
+pub type comp_t = u16;
+pub type comp2_t = u32;
+// ===========================================================
+// Accounting Flags
+// The actual accounting
+// flags.
+pub const AFORK: u8     = 0x01; // Executed fork, but no exec.
+pub const ASU: u8       = 0x02; // Used super-user privilages.
+pub const ACOMPAT: u8   = 0x04; // Used compatibility mode.
+pub const ACORE: u8     = 0x08; // Dumped core
+pub const AXSIG: u8     = 0x10; // Killed by a signal
+pub const AGROUP: u8    = 0x20; // Was the last task of the process
+                                // group.
+
+
+pub const ACCT_COMM: usize = 16;
