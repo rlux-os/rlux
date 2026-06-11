c9812c262abfe994587a07c6da73a40077e721c6
allexanderxha <allexanderxha@icloud.com>
2026-06-11 09:28:08 +0000

    * Boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+#[no_mangle] // Don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // kernel logic will begin here
+    loop {}
 }
